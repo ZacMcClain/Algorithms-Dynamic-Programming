3ceb2acbc764a2cbd63adece6fcc2e2e20949f1e
ZacMcClain <zamcclai@mines.edu>
2015-10-27 16:34:37 -0600

    Init commit, added a few exmple files and a loading system

---
+++ README.md
@@ -0,0 +1,40 @@
+
+#Title: Dynamic Programming
+##CSCI406 Assignment 2
+
+## Program Information
+
+*Names: 
+1.Brandon Maurice Parrish
+2.Christopher Travis Johnson
+3.Zac McClain
+*Date: 10/27/2015
+*Course: CSCI406 Algorithms
+*Professor: Dr. Dinesh Mehta
+
+##Source file(s):
+	src/main.cpp
+
+##Description:
+	
+
+##Usage:
+	~/path/to/main.cpp $ ./dymProg ../inputs/ex1.txt
+
+##Build Instructions:
+	~/path/to/workingDir $ make
+
+##Files:
+Files must observe the fallowing format: Comments are denoted by a '%' symbol
+at the beginning of a line. Full line comments only, Comments are contiguous and only
+found at the beginning of the program. The first data line will always contain the
+number of data entries in the file. All subsequent lines will be interpreted as data.
+
+##Notes:
+###Bugs:
+There is a small chance that your terminal does not support ANSI codes, then
+you will only see gibberish instead of output text.
+
+###Implementation Details:
+Please see in the comments in my source code for more details.
+
---
+++ src/main.cpp
@@ -0,0 +1,155 @@
+
+
+// System
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cmath>
+
+// Use colored text outputs
+static const bool COLOR_ON = true;
+
+using namespace std;
+
+// Text outputs
+void outputColoredText( string message, int color, bool newLine )
+{
+	if( COLOR_ON ) {
+		if( newLine ) {
+			cout << "\x1b[3" << color << ";1m" << message << "\x1b[0m" << endl;
+		} else {
+			cout << "\x1b[3" << color << ";1m" << message << "\x1b[0m";
+		}
+	} else {
+		if( newLine ) {
+			cout <<  message << endl;
+		} else {
+			cout << message;
+		}
+	}
+}
+
+// Takes a message then two colors both [0,7] the first is the text color the next
+// is the background color fallowed by a boolean, true if a new line is needed.
+void outputColoredTandB( string message, int textColor, int bgColor, bool newLine )
+{
+	if( COLOR_ON ) {
+		if( newLine ) {
+			cout << "\x1b[3" << textColor << ";4" << bgColor << ";1m " 
+				<< message << " \x1b[0m" << endl;
+		} else {
+			cout << "\x1b[3" << textColor << ";4" << bgColor << ";1m "
+				<< message << " \x1b[0m";
+		}
+	} else {
+		if( newLine ) {
+			cout <<  message << endl;
+		} else {
+			cout << message;
+		}
+	}
+}
+
+void outputMessage( string message, char* info_var = NULL )
+{
+	if( info_var == NULL ) {
+		outputColoredText( message, 2, true );
+	} else { 
+		outputColoredText( message, 2, false );
+		outputColoredTandB( info_var, 7, 0, true );
+	}
+}
+
+void outputWarning( string err_message, char* err_var = NULL )
+{
+	if( err_var == NULL ) {
+		outputColoredText( err_message, 1, true );
+	} else {
+		outputColoredText( err_message, 1, false );
+		outputColoredTandB( err_var, 7, 0, true );
+	}
+}
+
+template <typename T> string numberToString( T Number )
+{
+    ostringstream ss;
+    ss << Number;
+    return( ss.str() );
+}
+
+void printArray( int* stringAddr, int arrayLen, string name ) 
+{
+  cout << name << ": [";
+  for( int i = 0; i < arrayLen; i++ ) {
+    if( i == (arrayLen-1) ) {
+      cout << stringAddr[i];
+    } else {
+      cout << stringAddr[i] << ", ";
+    } 
+  }
+  cout << "]" << endl;
+}
+
+int main( int argc, char** argv )
+{
+	FILE *filePointer;
+  	char line[1024];
+  	char* fileName = argv[1];
+  	int numberOfDays; // aka n
+  	int* sArray; // s0...sn
+  	int* xArray; // x0...xn
+
+  	// ensure that only one argument has been provided
+  	if( argc != 2 ) {
+		outputWarning( "Unsupported number of arguments where provided!" );
+		outputMessage( "Usage example: ", (char*)"workingDir/ $ ./dynProg ex1.txt" );
+		outputMessage( "All files must be in the correct format." );
+		exit( 1 );
+	}
+
+  	// Check that the provided argument is a file that can be opened.
+	if( (filePointer = fopen(fileName, "r") ) == NULL) {
+		outputWarning( "Could not open file: ", fileName );
+		exit( 1 );
+	}
+	outputMessage( "Reading in data from file: ", fileName );
+
+	// ignore comments.
+	fgets( line, 128, filePointer );
+	while (line[0] == '%') {
+		fgets( line, 128, filePointer ); 
+	}
+
+	// Read number of rows (rowCount), number of columns (columnCount) and
+	sscanf( line, "%d\n", &numberOfDays );
+
+	// Allocate memory;
+	sArray = new int[numberOfDays];
+	xArray = new int[numberOfDays];
+
+	// readIn the rest of the array
+	for( int i = 0; i < numberOfDays; i++ ) {
+		fscanf( filePointer, "%d %d\n", &(sArray[i]), &(xArray[i]) );
+	}
+
+	// Close the file.
+	fclose( filePointer );
+
+	// Output Arrays to insure correctness of loading:
+	cout << "N: " << numberOfDays << endl;
+	if( numberOfDays < 40 ) {
+		printArray( sArray, numberOfDays, "S" );
+		printArray( xArray, numberOfDays, "X" );
+	}
+
+	// Deallocate memory
+	delete[] sArray;
+	sArray = NULL;
+	delete[] xArray;
+	xArray = NULL;		
+
+	// exit with success code 0
+	return( 0 );
+}
