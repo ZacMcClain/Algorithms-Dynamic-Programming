bc01fdc001ecbc8bfebd4ed8b9accd53e5dfe1d0
Travis Johnson <cjohnso0@mymail.mines.edu>
2015-11-03 10:42:31 -0700

    Added some comments, did a little refactoring

--- DP.rb
+++ DP.rb
@@ -1,3 +1,4 @@
+# initialize_table will populate a 2D array (table) with an initial row of 0 values
 def initialize_table(given,  can)
 	table = Array.new(can.length + 1)
     (0...(can.length + 1)).each { |x| table[x] = Array.new(given.length) }
@@ -5,6 +6,7 @@ def initialize_table(given,  can)
 	return table	
 end
 
+# output the structure of the table to the console
 def print_table table
 	table.each do |x|
 		if x != nil
@@ -14,16 +16,18 @@ def print_table table
 	end
 end	
 
-def min(x, y) 
-	if x < y
+# return limiting factor (amount of data or server processing power)
+def min(x, s) 
+	if x < s
 		return x
 	end
  	
-	return y
+	return s
 end
 
+# return the maximum value of a specific column in a table
+# linear search causes O(n)
 def column_max( table, column )
-	
 	max = -9999999
 	(0...(column + 2)).each do |x|
 		if table[x][column] != nil
@@ -36,40 +40,43 @@ def column_max( table, column )
 	return max
 end
 		
-
+# process the data sets X (given) and S (can)
+# two iterative loops causes O(n^2)?
 def make_table( given, can )
-	table = initialize_table(given, can)
-	
+	table = initialize_table(given, can)	# init row of 0s
+
+	# need one more row than columns (n + 1) rows
 	can = [0] + can
 
 	(0...(given.length)).each do |x|
-		(1...(can.length + 1)).each do |y|
+		(1...(can.length + 1)).each do |s|
 
-			if y - x < 2
+			# do not calculate min for lower diagonals
+			# cannot possibly process x_1 with the power of s_2, nor x_2 with s_3
+			if s - x < 2	
 				
-				if x - 1 < 0 || y - 1 < 0 
-					table[y][x] = min( given[x], can[y] )
+				if x - 1 < 0 || s - 1 < 0 
+					table[s][x] = min( given[x], can[s] )	# table[1][0] is min( x_1, s_1 )
 				
-				elsif y - x >=  0
-						
-					table[y][x] = min( given[x], can[y] ) + table[ y - 1 ][ x - 1 ] 
-				elsif y == 1
+				elsif s - x >=  0
+					table[s][x] = min( given[x], can[s] ) + table[ s - 1 ][ x - 1 ] 
+					# calculate sum of diagonals
 
-					table[y][x] = min( given[x], can[y] ) + table[ y - 1 ][ x - 1 ] + column_max(table, x - y - 1 )
+				elsif s == 1
+
+					table[s][x] = min( given[x], can[s] ) + table[ s - 1 ][ x - 1 ] + column_max(table, x - s - 1 )
+					# calculate sum of diagonals + max of the column immediately left of a reboot
 
 				else 
-					table[y][x] = min( given[x], can[y] ) + table[ y - 1 ][ x - 1]
+					table[s][x] = min( given[x], can[s] ) + table[ s - 1 ][ x - 1]
+					# what does this do differently from the second condition?
+
 				end
-
-						
-				
 			end 
-					
 		end
-
 	end
 
- 	print_table table
+ 	print_table table	# output the table
 	return table
 end		
 					
